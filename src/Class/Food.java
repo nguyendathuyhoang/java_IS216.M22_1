1874ca95a2c1c2bc8d0a3843790365b7dc990079
'nguyendathuyhoang' <'hoanghonghach16@gmail.com'>
2022-06-04 14:05:34 +0700

    HomePage interface; form food, drink, service and process of adding, editing, deleting of these three forms.

---
+++ src/Class/Food.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author Nguyễn Đạt Huy Hoàng
+ * Lớp Food
+ */
+public class Food {
+    private String food_id;
+    // Mã món ăn
+    private String food_name;
+    // Tên món ăn
+    private int food_price;
+    // Giá món ăn
+
+    public String getFood_id() {
+        return food_id;
+    }
+
+    public void setFood_id(String food_id) {
+        this.food_id = food_id;
+    }
+
+    public String getFood_name() {
+        return food_name;
+    }
+
+    public void setFood_name(String food_name) {
+        this.food_name = food_name;
+    }
+
+    public int getFood_price() {
+        return food_price;
+    }
+
+    public void setFood_price(int food_price) {
+        this.food_price = food_price;
+    }
+    
+}
