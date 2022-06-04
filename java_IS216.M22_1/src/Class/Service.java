8dc513abac77fab1c43571670ad00ae122d9b2e9
nguyendathuyhoang <73891386+nguyendathuyhoang@users.noreply.github.com>
2022-06-05 02:45:46 +0700

    New file Project

---
+++ java_IS216.M22_1/src/Class/Service.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author Nguyễn Đạt Huy Hoàng
+ * Lớp service
+ */
+public class Service {
+    private String service_id;
+    // Mã dịch vụ khách sạn
+    private String service_name;
+    // Tên dịch vụ khách sạn
+    private int service_price;
+    // Giá dịch vụ khách sạn
+
+    public String getService_id() {
+        return service_id;
+    }
+
+    public void setService_id(String service_id) {
+        this.service_id = service_id;
+    }
+
+    public String getService_name() {
+        return service_name;
+    }
+
+    public void setService_name(String service_name) {
+        this.service_name = service_name;
+    }
+
+    public int getService_price() {
+        return service_price;
+    }
+
+    public void setService_price(int service_price) {
+        this.service_price = service_price;
+    }
+    
+    
+}
---
+++ java_IS216.M22_1/src/Database/FoodDb.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+import Class.Food;
+import java.sql.*;
+import javax.swing.AbstractAction.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Nguyễn Đạt Huy Hoàng
+ * Kết nối Database bảng đồ ăn
+ */
+public class FoodDb {
+    Connection conn = DataConnection.Connect();
+    PreparedStatement stat = null;
+    ResultSet rs = null;
+    
+    public void insertFood(Food food)
+    {
+        try
+        {
+            String insertFood = "insert into doan(madoan,tendoan,giadoan) values ('"+food.getFood_id()+"','"+food.getFood_name()+"',"+food.getFood_price()+")";
+            stat =conn.prepareStatement(insertFood);
+            stat.execute();
+            JOptionPane.showMessageDialog(null, "Thêm thành công!");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            JOptionPane.showMessageDialog(null, "Thêm thất bại!");
+        }
+        finally
+        {
+            flushStatementOnly();
+        }        
+    }
+    
+    public ResultSet getFood()
+    {
+        try
+        {
+            String query = "select * from doan";
+            stat = conn.prepareStatement(query);
+            rs = stat.executeQuery();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            JOptionPane.showMessageDialog(null, "Lỗi!");
+        }
+        return rs;
+    }
+    
+    public void updateFood(Food food)
+    {
+        try
+        {
+            String updateFood = "update doan set tendoan = '" + food.getFood_name()+"', giadoan = "+food.getFood_price()+" where madoan ='"
+                    + food.getFood_id()+"'";
+            stat = conn.prepareStatement(updateFood);
+            stat.execute();
+            JOptionPane.showMessageDialog(null, "Cập nhật thành công!!!");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            JOptionPane.showMessageDialog(null, "Cập nhất thất bại!!!");
+        }
+        finally
+        {
+            flushStatementOnly();
+        }
+    }
+    
+    public void deleteFood(String foodId)
+    {
+        try
+        {
+            String deleteFood = "delete from doan where madoan = '"+foodId+"'";
+            stat = conn.prepareStatement(deleteFood);
+            stat.execute();
+            JOptionPane.showMessageDialog(null, "Xóa thành công!!!");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            System.out.println("Xóa thất bại!!!");
+        }
+        finally
+        {
+            flushStatementOnly();
+        }
+    }
+    
+    public void flushAll()
+    {
+        try
+        {
+            stat.close();
+            rs.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Closing DataBase!!!");
+        }
+    }
+    private void flushStatementOnly()
+    {
+        try
+        {
+            stat.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Closing DataBase!");
+        }
+    }
+}
